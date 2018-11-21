12d8d4ad33c90aa09b895f3d871bf9533255f21e
t-k- <clock126@126.com>
2018-11-21 12:33:18 -0500

    FIX clear_feeds.sh

--- clear_feeds.sh
+++ clear_feeds.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-DIR=test
-find $DIR/ -name '*.feed.json' | xargs rm -f
-find $DIR/ -name '_list_.json' | xargs rm -f
+#!/bin/bash
+DIR=feeds
+find $DIR/ -name '*.feed.json' -print0 | xargs -0 rm -f
+find $DIR/ -name '_list_.json' -print0 | xargs -0 rm -f
 git co $DIR/
