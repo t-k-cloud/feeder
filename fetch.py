b8e8e4f938f26e9f1ead89c16ed13d8fef4f46dc
t-k- <clock126@126.com>
2018-12-02 00:16:43 -0500

    record last-updated time.

--- fetch.py
+++ fetch.py
@@ -24,32 +24,35 @@ def fetch(url):
 	if not len(feeds.entries) > 0:
 		raise Exception('failed')
 	# update title and link
-	title, link = "(no title)", url
+	title, link, updated = "(no title)", url, ""
+
 	if hasattr(feeds['feed'], 'title'):
 		title = feeds['feed']['title']
 	if hasattr(feeds['feed'], 'link'):
 		link = feeds['feed']['link']
+	if hasattr(feeds['feed'], 'updated'):
+		updated = feeds['feed']['updated']
 	# update feed entries
 	entries = []
 	for ent in feeds.entries:
 		entries.append({
 			"title": ent.title if hasattr(ent, 'title') else "",
 			"link": ent.link if hasattr(ent, 'link') else "",
+			"updated": ent.updated if hasattr(ent, 'updated') else "",
 			"desc": ent.description if hasattr(ent, 'description') else ""
 		})
-	return title, link, entries
+	return title, link, updated, entries
 
 def write_feeds(dirname, entries, recent):
 	any_update = 0
 	# write JSON feeds to files
 	for ent in entries:
 		if (len(ent["title"]) == 0 or
-		    len(ent["link"])  == 0 or
-		    len(ent["desc"])  == 0):
+		    len(ent["link"])  == 0):
 			continue
 		link = ent["link"]
 		if link not in recent:
-			ent['fetch-time'] = str(datetime.datetime.now())
+			ent['fetched'] = str(datetime.datetime.now())
 			p = dirname + '/' + str(uuid.uuid1()) + '.feed.json'
 			with open(p, 'w') as fh:
 				json.dump(ent, fh)
@@ -79,7 +82,7 @@ def process_feed_file(path):
 			print("[no url]", flush=True)
 			return j
 		try:
-			title, link, entries = fetch(j['url'])
+			title, link, updated, entries = fetch(j['url'])
 		except KeyboardInterrupt:
 			print("Aborted by KeyboardInterrupt.")
 			exit(1)
@@ -87,13 +90,14 @@ def process_feed_file(path):
 			j['failed'] += 1
 			print("<* Failed *>", flush=True)
 			return j
-		ups = write_feeds(dirname, entries, j['recent'])
-		if ups: j['last-update'] = str(datetime.datetime.now())
+		n_new = write_feeds(dirname, entries, j['recent'])
+		if n_new: j['last-fetched'] = str(datetime.datetime.now())
 		j['title'] = title
 		j['link'] = link
+		j['last-updated'] = updated
 		j['view-engine'] = 'feed-view' # support listify
 		j['detailed'] = True # support listify
-		print("[%s] %d updates" % (title, ups), flush=True)
+		print("[%s] %d updates" % (title, n_new), flush=True)
 		return j
 
 print('[fetch feeds]', SRCH_PATH, flush=True)
@@ -120,6 +124,7 @@ with open(FEED_LIST + '.tmp', 'w') as feed_list_fh:
 			continue
 		# append to feed list
 		print(tag, j['url'], file=feed_list_fh)
+		time.sleep(1)
 		feed_list_fh.flush()
 
 # savely overwrite
